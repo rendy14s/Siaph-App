dcaab6eeb1ec132d384098886ae7f790782cbdc9
Rendy Sutandy <rendy.sutandy@dataon.com>
2017-08-15 17:05:09 +0700

    change function non default home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,85 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    private toast: ToastController
+  ) {
 
+  }
+
+  public perda() {
+    console.log('klik');
+    this.navCtrl.push('PerdaPage');
+  }
+
+  public perwal() {
+    console.log('klik');
+    this.navCtrl.push('PerwalPage');
+  }
+
+  public tracking() {
+    let toast = this.toast.create({
+      message: 'Under Development',
+      duration: 3000,
+      position: 'bottom'
+    });
+    toast.present();
+  }
+
+  public galeri() {
+    console.log('klik');
+    this.navCtrl.push('GaleriPage');
+  }
+
+  public kontak() {
+    let toast = this.toast.create({
+      message: 'Under Development',
+      duration: 3000,
+      position: 'bottom'
+    });
+    toast.present();
+  }
+
+  public account() {
+    let toast = this.toast.create({
+      message: 'Under Development',
+      duration: 3000,
+      position: 'bottom'
+    });
+    toast.present();
+  }
+
+  public update() {
+    let toast = this.toast.create({
+      message: 'Under Development',
+      duration: 3000,
+      position: 'bottom'
+    });
+    toast.present();
+  }
+
+  public profil() {
+    let toast = this.toast.create({
+      message: 'Under Development',
+      duration: 3000,
+      position: 'bottom'
+    });
+    toast.present();
+  }
+
+  public perpus() {
+    let toast = this.toast.create({
+      message: 'Under Development',
+      duration: 3000,
+      position: 'bottom'
+    });
+    toast.present();
+  }
+
+  public kepwal() {
+    console.log('klik');
+    this.navCtrl.push('KepwalPage');
   }
 
 }
