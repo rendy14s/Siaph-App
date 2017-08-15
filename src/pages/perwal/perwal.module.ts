040426890e59929ce431e4237c00d5e9db4b63a9
Rendy Sutandy <rendy.sutandy@dataon.com>
2017-08-15 17:05:58 +0700

    add perwal page

---
+++ src/pages/perwal/perwal.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { PerwalPage } from './perwal';
+
+@NgModule({
+  declarations: [
+    PerwalPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(PerwalPage),
+  ],
+})
+export class PerwalPageModule {}
