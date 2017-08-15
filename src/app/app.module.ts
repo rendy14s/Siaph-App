9f35ea74bd29505a564417edec1990bd6709ff73
Rendy Sutandy <rendy.sutandy@dataon.com>
2017-08-15 17:02:47 +0700

    add Filetransfer plugin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { HttpModule } from '@angular/http';
+import { FileTransfer } from '@ionic-native/file-transfer';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
