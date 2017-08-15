cb5f02e742e8b99648ffdc6b7d925166791a376c
Rendy Sutandy <rendy.sutandy@dataon.com>
2017-08-15 17:05:47 +0700

    add perpus page

---
+++ src/pages/perpus/perpus.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { PerpusPage } from './perpus';
+
+@NgModule({
+  declarations: [
+    PerpusPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(PerpusPage),
+  ],
+})
+export class PerpusPageModule {}
