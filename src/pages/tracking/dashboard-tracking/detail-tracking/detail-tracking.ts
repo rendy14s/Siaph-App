22c6992baf44bc847093ebde6ac182f4aabc8678
Ren <rendy.sutandy@dataon.com>
2017-11-07 03:29:30 +0700

    add function get data documents

--- src/pages/tracking/dashboard-tracking/detail-tracking/detail-tracking.ts
+++ src/pages/tracking/dashboard-tracking/detail-tracking/detail-tracking.ts
@@ -1,8 +1,9 @@
 import { Component, ViewChild } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController, AlertController } from 'ionic-angular';
 import { SiaphTrackingdocumentsApi } from './../../../../shared/sdk/services/custom/SiaphTrackingdocuments';
 import { Storage } from '@ionic/storage';
 import { SiaphDepthroleApi } from './../../../../shared/sdk/services/custom/SiaphDepthrole';
+import { SiaphDocumentsApi } from './../../../../shared/sdk/services/custom/SiaphDocuments';
 /**
  * Generated class for the DetailTrackingPage page.
  *
@@ -28,13 +29,24 @@ export class DetailTrackingPage {
   public arrayLength: any;
   public showCss: boolean;
 
+
+  public dataDoc: any;
+  public docNo: any;
+  public docSubject: any;
+  public agendaNo: any;
+
+  public statusDisposisi: any;
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public siaphTrackingdocumentsApi: SiaphTrackingdocumentsApi,
     public storage: Storage,
-    public siaphDepthroleApi: SiaphDepthroleApi
+    public siaphDepthroleApi: SiaphDepthroleApi,
+    public siaphDocumentsApi: SiaphDocumentsApi,
+    public loadingCtrl: LoadingController,
+    public alertCtrl: AlertController
   ) {
+
     setTimeout(() => {
       this.goToSlide();
     }, 500);
@@ -46,14 +58,24 @@ export class DetailTrackingPage {
 
   ionViewDidLoad() {
     this.idDoc = this.navParams.get('idDoc');
+    this.getDataDocuments();
+
+    let loadPub = this.loadingCtrl.create({
+      content: 'Please wait...'
+    });
+    loadPub.present();
+
     this.siaphTrackingdocumentsApi.find({
       where: {
         idDoc: this.idDoc
       }
     }).subscribe(result1 => {
       this.dataTracking = result1;
+      console.log(this.dataTracking, 'Data Tracking');
       this.dataTrackingLength = this.dataTracking.length;
-
+      const dataLast = this.dataTracking[this.dataTrackingLength - 1];
+      this.statusDisposisi = dataLast.statusDisposisi;
+      console.log(this.statusDisposisi);
       this.siaphDepthroleApi.find({
         where: {
           idRole: this.dataTracking[0]['fromDoc']
@@ -61,6 +83,8 @@ export class DetailTrackingPage {
       }).subscribe((result2) => {
         result2['RoleFrom'] = result2[0]['nameRole'];
         this.dataTemp = result2;
+
+        loadPub.dismiss();
 
         for (let i = 0; i < this.dataTrackingLength; i++) {
           this.siaphDepthroleApi.find({
@@ -74,6 +98,37 @@ export class DetailTrackingPage {
         }
       });
     });
+  }
+
+  public getDataDocuments() {
+    this.siaphDocumentsApi.findById(this.idDoc).subscribe((result) => {
+      this.dataDoc = result;
+      console.log(this.dataDoc, 'Doc Data');
+
+      this.docNo = this.dataDoc.noDoc;
+      this.docSubject = this.dataDoc.subjectDoc;
+      this.agendaNo = this.dataDoc.noAgendaDoc;
+    }, error => {
+      let alert = this.alertCtrl.create({
+        subTitle: " No Internet Access! Cek your connection",
+        buttons: [
+          {
+            text: 'Dismiss',
+            handler: () => {
+              this.navCtrl.setRoot('DashboardTrackingPage');
+            }
+          }]
+      });
+      alert.present();
+    });
+  }
+
+  public approved() {
+    console.log('Approve');
+  }
+
+  public cancel() {
+    this.navCtrl.setRoot('DashboardTrackingPage');
   }
 
 }
