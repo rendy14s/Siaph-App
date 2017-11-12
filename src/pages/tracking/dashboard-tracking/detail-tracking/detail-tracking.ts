4d41c2ed237cb4bab728abf5805acd336078d300
Ren <rendy1429@gmail.com>
2017-11-13 02:52:45 +0700

    add logic give coment document and bug fixing approved document

--- src/pages/tracking/dashboard-tracking/detail-tracking/detail-tracking.ts
+++ src/pages/tracking/dashboard-tracking/detail-tracking/detail-tracking.ts
@@ -1,11 +1,11 @@
 import { Component, ViewChild } from '@angular/core';
-import { IonicPage, NavController, NavParams, LoadingController, AlertController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController, AlertController, ToastController } from 'ionic-angular';
 import { SiaphTrackingdocumentsApi } from './../../../../shared/sdk/services/custom/SiaphTrackingdocuments';
 import { Storage } from '@ionic/storage';
 import { SiaphDepthroleApi } from './../../../../shared/sdk/services/custom/SiaphDepthrole';
 import { SiaphDocumentsApi } from './../../../../shared/sdk/services/custom/SiaphDocuments';
+import { SiaphNoteddocumentsApi } from './../../../../shared/sdk/services/custom/SiaphNoteddocuments';
 import moment from 'moment';
-import { FormBuilder, Validators } from '@angular/forms';
 /**
  * Generated class for the DetailTrackingPage page.
  *
@@ -19,6 +19,7 @@ import { FormBuilder, Validators } from '@angular/forms';
   templateUrl: 'detail-tracking.html',
 })
 export class DetailTrackingPage {
+  
 
   @ViewChild('mySlider') mySlider: any;
 
@@ -47,6 +48,11 @@ export class DetailTrackingPage {
   public dataDepthRole: any;
   public selectto: any;
   public disposisiForm: any;
+
+  public shiwFinish: boolean;
+  public showFinish: boolean;
+  public datax;
+  public noted: any;
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
@@ -56,12 +62,27 @@ export class DetailTrackingPage {
     public siaphDocumentsApi: SiaphDocumentsApi,
     public loadingCtrl: LoadingController,
     public alertCtrl: AlertController,
-    public fb: FormBuilder,
+    public toastCtrl: ToastController,
+    public siaphNoteddocumentsApi: SiaphNoteddocumentsApi
   ) {
-
     setTimeout(() => {
       this.goToSlide();
     }, 500);
+  }
+
+  goToSlide() {
+    this.mySlider.slideTo(this.arrayLength, 500);
+  }
+
+  ionViewDidLoad() {
+    this.idDoc = this.navParams.get('idDoc');
+
+    let loadPub = this.loadingCtrl.create({
+      content: 'Please wait...'
+    });
+    loadPub.present();
+
+    this.getDataDocuments();
 
     this.storage.ready().then(() => {
       this.storage.get('siaphCredential').then((siaphCredential) => {
@@ -73,68 +94,55 @@ export class DetailTrackingPage {
         this.siaphDepthroleApi.find().subscribe((result) => {
           this.dataDepthRole = result;
           console.log(this.dataDepthRole, 'Data Role');
-          // this.selectto = this.dataDepthRole[6].nameRole;
         });
-      });
-    });
 
-    this.disposisiForm = this.fb.group({
-      'selectto': ['TIM PEMBAHASAN RAPERWAL', Validators.compose([Validators.required])],
-      // 'nodoc': ['', Validators.compose([Validators.required])],
-      // 'receiptDate': [this.receiptDate, Validators.compose([Validators.required])],
-      // 'noAgenda': ['', Validators.compose([Validators.required])],
-      // 'subject': ['', Validators.compose([Validators.required])],
-      // 'noted': ['', Validators.compose([Validators.required])],
-    });
+        this.siaphTrackingdocumentsApi.find({
+          where: {
+            idDoc: this.idDoc
+          }
+        }).subscribe(result1 => {
+          this.dataTracking = result1;
+          console.log(this.dataTracking, 'Data Tracking');
+          this.dataTrackingLength = this.dataTracking.length;
+          const dataLast = this.dataTracking[this.dataTrackingLength - 1];
+          this.statusDisposisi = dataLast.statusDisposisi;
+          console.log(this.statusDisposisi);
 
-  }
-
-  goToSlide() {
-    this.mySlider.slideTo(this.arrayLength, 500);
-  }
-
-  ionViewDidLoad() {
-    this.idDoc = this.navParams.get('idDoc');
-    this.getDataDocuments();
-
-    let loadPub = this.loadingCtrl.create({
-      content: 'Please wait...'
-    });
-    loadPub.present();
-
-    this.siaphTrackingdocumentsApi.find({
-      where: {
-        idDoc: this.idDoc
-      }
-    }).subscribe(result1 => {
-      this.dataTracking = result1;
-      console.log(this.dataTracking, 'Data Tracking');
-      this.dataTrackingLength = this.dataTracking.length;
-      const dataLast = this.dataTracking[this.dataTrackingLength - 1];
-      this.statusDisposisi = dataLast.statusDisposisi;
-      console.log(this.statusDisposisi);
-      this.siaphDepthroleApi.find({
-        where: {
-          idRole: this.dataTracking[0]['fromDoc']
-        }
-      }).subscribe((result2) => {
-        result2['RoleFrom'] = result2[0]['nameRole'];
-        this.dataTemp = result2;
-
-        loadPub.dismiss();
-
-        for (let i = 0; i < this.dataTrackingLength; i++) {
           this.siaphDepthroleApi.find({
             where: {
-              idRole: this.dataTracking[i].toDoc
+              idRole: this.dataTracking[0]['fromDoc']
             }
-          }).subscribe((result3) => {
-            this.dynamicData[i] = result3[0]['nameRole'];
-            this.arrayLength = this.dynamicData.length - 1;
+          }).subscribe((result2) => {
+            result2['RoleFrom'] = result2[0]['nameRole'];
+            this.dataTemp = result2;
+
+            if(this.statusDisposisi == 'Closed') {
+              this.showFinish = true;
+              this.shiwFinish = false;
+            } else if (this.statusDisposisi == 'Open') {
+              this.showFinish = false;
+              this.shiwFinish = true;
+            }
+
+            loadPub.dismiss();
+
+            for (let i = 0; i < this.dataTrackingLength; i++) {
+              this.siaphDepthroleApi.find({
+                where: {
+                  idRole: this.dataTracking[i].toDoc
+                }
+              }).subscribe((result3) => {
+                this.dynamicData[i] = result3[0]['nameRole'];
+                this.arrayLength = this.dynamicData.length - 1;
+              });
+            }
           });
-        }
+        });
+
+
       });
     });
+
   }
 
   public getDataDocuments() {
@@ -162,22 +170,114 @@ export class DetailTrackingPage {
 
   public approved() {
     console.log('Approve');
+    if (this.selectto == null || this.selectto == undefined) {
+      let alert = this.alertCtrl.create({
+        title: 'ATTENTION PLEASE!',
+        message: 'Column "Letter To" is empty! If this true. The Document will be automatic status is Closed or Fully Approved and be noted name by you. ',
+        buttons: [
+          {
+            text: 'Disagree',
+            handler: () => {
+              console.log('Disagree clicked');
+            }
+          },
+          {
+            text: 'Agree',
+            handler: () => {
+              const dataApproved = {
+                idDoc: this.dataTracking[0].idDoc,
+                fromDoc: this.idStorage,
+                toDoc: this.idStorage,
+                statusDisposisi: 'Closed',
+                prosesDate: this.formatDate(),
+                editedDate: this.formatDate(),
+                editedBy: this.userName
+              };
 
-    const dataApproved = {
-      idDoc: this.dataTracking.idDoc,
-      fromDoc: this.idStorage,
-      toDoc: this.disposisiForm.controls.selectto.value,
-      statusDisposisi: 'Open',
-      prosesDate: this.formatDate(),
-      editedDate: this.formatDate(),
-      editedBy: this.userName
-    };
+              this.siaphTrackingdocumentsApi.create(dataApproved).subscribe((result) => {
+                console.log(result);
+                this.datax = result;
+                const dataNoted = {
+                  idTracking: this.datax.idTracking,
+                  idDoc: this.datax.idDoc,
+                  dateNoted: this.formatDate(),
+                  notesDoc: this.noted
+                };
+                if(this.noted == null || this.noted == undefined) {
+                  let toast = this.toastCtrl.create({
+                    message: 'Success Disposisi!',
+                    duration: 3000,
+                    position: 'bottom'
+                  });
+                  toast.present();
+                  this.navCtrl.setRoot('DashboardTrackingPage');
+                } else {
+                  this.siaphNoteddocumentsApi.create(dataNoted).subscribe((result) => {
+                    let toast = this.toastCtrl.create({
+                      message: 'Success Disposisi!',
+                      duration: 3000,
+                      position: 'bottom'
+                    });
+                    toast.present();
+                    this.navCtrl.setRoot('DashboardTrackingPage');
+                  })
+                }
+              }, (error) => {
+                console.log(error, 'Error Approved');
+              });
 
-    this.siaphTrackingdocumentsApi.create(dataApproved).subscribe((result) => {
-      console.log(result);
-    }, (error) => {
-      console.log(error, 'Error Approved');
-    });
+            }
+          }
+        ]
+      });
+      alert.present();
+    } else {
+
+      const dataApproved = {
+        idDoc: this.dataTracking[0].idDoc,
+        fromDoc: this.idStorage,
+        toDoc: this.selectto,
+        statusDisposisi: 'Open',
+        prosesDate: this.formatDate(),
+        editedDate: this.formatDate(),
+        editedBy: this.userName
+      };
+
+      this.siaphTrackingdocumentsApi.create(dataApproved).subscribe((result) => {
+        console.log(result);
+        this.datax = result;
+        const dataNoted = {
+          idTracking: this.datax.idTracking,
+          idDoc: this.datax.idDoc,
+          dateNoted: this.formatDate(),
+          notesDoc: this.noted
+        };
+        
+        if(this.noted == null || this.noted == undefined) {
+          let toast = this.toastCtrl.create({
+            message: 'Success Disposisi!',
+            duration: 3000,
+            position: 'bottom'
+          });
+          toast.present();
+          this.navCtrl.setRoot('DashboardTrackingPage');
+        } else {
+          this.siaphNoteddocumentsApi.create(dataNoted).subscribe((result) => {
+            let toast = this.toastCtrl.create({
+              message: 'Success Disposisi!',
+              duration: 3000,
+              position: 'bottom'
+            });
+            toast.present();
+            this.navCtrl.setRoot('DashboardTrackingPage');
+          })
+        }
+       
+      }, (error) => {
+        console.log(error, 'Error Approved');
+      });
+
+    }
   }
 
   public formatDate() {
