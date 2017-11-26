cb1182baa622a6aa95bca40e9737d634133beb53
Ren <rendy1429@gmail.com>
2017-11-27 02:30:28 +0700

    change logic into people

--- src/pages/tracking/people-detail-view/people-detail-view.module.ts
+++ src/pages/tracking/people-detail-view/people-detail-view.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { PeopleDetailViewPage } from './people-detail-view';
+import { RelativeTime } from '../../../pipe/relative-time';
 
 @NgModule({
   declarations: [
     PeopleDetailViewPage,
+    RelativeTime
   ],
   imports: [
     IonicPageModule.forChild(PeopleDetailViewPage),
--- src/pages/tracking/people-detail-view/people-detail-view.ts
+++ src/pages/tracking/people-detail-view/people-detail-view.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams, ViewController, AlertController, L
 import { SiaphDocumentsApi } from './../../../shared/sdk/services/custom/SiaphDocuments';
 import { SiaphTrackingdocumentsApi } from './../../../shared/sdk/services/custom/SiaphTrackingdocuments';
 import { SiaphDepthroleApi } from './../../../shared/sdk/services/custom/SiaphDepthrole';
+
 /**
  * Generated class for the PeopleDetailViewPage page.
  *
@@ -17,8 +18,11 @@ import { SiaphDepthroleApi } from './../../../shared/sdk/services/custom/SiaphDe
 })
 export class PeopleDetailViewPage {
   public dataDoc: any;
-
   public tracking: any = 'status';
+
+  public dataTracking: any;
+  public dataTrackingLength: any;
+  public dataFirst: any;
 
   constructor(
     public navCtrl: NavController,
@@ -34,6 +38,29 @@ export class PeopleDetailViewPage {
 
   ionViewDidLoad() {
     this.dataDoc = this.navParams.get('data');
+    console.log(this.dataDoc, 'DATA DOC');
+    this.siaphTrackingdocumentsApi.find({
+      where: {
+        idDoc: this.dataDoc
+      }
+    }).subscribe(resultTracking => {
+      this.dataTracking = resultTracking;
+      console.log(this.dataTracking, 'DATA TRACKING');
+
+      this.dataTrackingLength = this.dataTracking.length;
+      this.siaphDepthroleApi.find({
+        where: {
+          idRole: this.dataTracking[0]['fromDoc']
+        }
+      }).subscribe(resultDeptFirst => {
+        console.log(resultDeptFirst, 'DEPTH ROLE 1');
+        this.dataFirst = resultDeptFirst;
+      });
+
+      // for (let i = 0; i < this.dataTrackingLength; i++) {
+      // }
+
+    });
   }
 
   public exit() {
